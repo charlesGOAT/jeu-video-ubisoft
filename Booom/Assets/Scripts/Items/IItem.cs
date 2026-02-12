39cf2698c0add1eaca71b275705d6908d6d6b424
Lea <lea.roy@outlook.com>
2026-02-12 15:44:15 -0500

    Item logic

---
+++ Booom/Assets/Scripts/Items/IItem.cs
@@ -0,0 +1,11 @@
+﻿
+public interface IItem
+{
+    public ItemType ItemType => ItemType.Basic;
+    public float ActiveTime => 5.0f; // basic time
+
+    public abstract void UseItem(Player player);
+    public abstract void PickupItem();
+
+    public abstract void UseTimeOver();
+}
