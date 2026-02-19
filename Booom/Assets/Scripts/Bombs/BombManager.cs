2e2c1bac18141bd58101a3f2aab8775dd6a2643d
Charles-Etienne Gauthier <charles-etienne.gauthier@outlook.com>
2026-02-19 13:33:19 -0500

    Ajout debuff tuile, placement de bombe revisite (hitbox et placer devant soi), Fix le bug de la tuile qui se fait hit 4 fois

--- Booom/Assets/Scripts/Bombs/BombManager.cs
+++ Booom/Assets/Scripts/Bombs/BombManager.cs
@@ -33,7 +33,7 @@ public class BombManager : MonoBehaviour
         {
             return;
         }
-
+        Vector3 bombHeight = Vector3.up * position.y;
         Vector2Int gridCoordinates = GridManagerStategy.WorldToGridCoordinates(position);
         Tile tile = GameManager.Instance.GridManager.GetTileAtCoordinates(gridCoordinates);
 
@@ -45,7 +45,7 @@ public class BombManager : MonoBehaviour
         Vector3 worldPosition = GridManagerStategy.GridToWorldPosition(gridCoordinates, tile.transform.position.y);
         bombPrefabs[(int)bombEnum - 1].associatedPlayer = playerEnum;
 
-        Instantiate(bombPrefabs[(int)bombEnum - 1], worldPosition, Quaternion.identity);
+        Instantiate(bombPrefabs[(int)bombEnum - 1], worldPosition + bombHeight, Quaternion.identity);
 
         _nextBombTime[playerEnum] = Time.time + bombCooldown;
     }
