89993e4cfbb554205a8f37cfa7f5ea0a03592c0b
Lea Roy <lea.roy@outlook.com>
2026-03-02 13:28:56 -0500

    fixed splash bomb

--- Booom/Assets/Scripts/Bombs/SplashBomb.cs
+++ Booom/Assets/Scripts/Bombs/SplashBomb.cs
@@ -5,6 +5,7 @@ public class SplashBomb : Bomb
     private readonly Vector2Int[] _offsets =
     {
         new Vector2Int(-1, -1),
+        new Vector2Int(-1, -2),
         new Vector2Int(0, -1),
         new Vector2Int(1, -1),
         new Vector2Int(-1, 0),
@@ -12,7 +13,9 @@ public class SplashBomb : Bomb
         new Vector2Int(1, 0),
         new Vector2Int(-1, 1),
         new Vector2Int(0, 1),
+        new Vector2Int(0, 2),
         new Vector2Int(1, 1),
+        new Vector2Int(2, 2),
     };
 
     protected override void PaintTiles()
@@ -23,30 +26,28 @@ public class SplashBomb : Bomb
 
         PlayerEnum currentOwner = bombTile.CurrentTileOwner;
         PlayerEnum newTileOwner = GameManager.Instance.IsSpreadingMode ? currentOwner : AssociatedPlayer;
-
+        
         foreach (var offset in _offsets)
         {
-            PaintTilesSurrounding(_bombCoordinates, offset, newTileOwner);
+            Vector2Int coords = _bombCoordinates + offset;
+            PaintTileSurrounding(coords, offset, newTileOwner);
+            HitPlayers(coords, offset);
         }
     }
 
-    private void PaintTilesSurrounding(Vector2Int bombCoordinates, Vector2Int offset, PlayerEnum tileOwner)
+    private void PaintTileSurrounding(Vector2Int coords, Vector2Int offset, PlayerEnum tileOwner)
     {
-        Vector2Int coords = bombCoordinates + offset;
         Tile tileToPaint = GameManager.Instance.GridManager.GetTileAtCoordinates(coords);
         if (tileToPaint == null) return;
 
         if (tileToPaint is Portal portalTile)
         {
-            PaintTilesSurrounding(portalTile.GetOtherPortalPosition(), offset, tileOwner);
+            PaintTileSurrounding(portalTile.GetOtherPortalPosition() + offset, offset, tileOwner);
             return;
         }
 
         if (tileToPaint.IsObstacle) return;
 
         tileToPaint.ChangeTileColor(tileOwner);
-
-        Vector2Int hitDirection = coords - _bombCoordinates;
-        HitPlayers(coords, hitDirection);
     }
 }
