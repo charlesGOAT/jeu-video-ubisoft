ec0b46304c2cf2fdf9bb0104704483452083ac34
Lea Roy <lea.roy@outlook.com>
2026-03-03 18:15:50 -0500

    Remove temp changes

--- Booom/Assets/Scripts/ScoreManager.cs
+++ Booom/Assets/Scripts/ScoreManager.cs
@@ -34,7 +34,7 @@ public class ScoreManager : MonoBehaviour
         int newScore = _acquiredTilesByPlayer[(int)player - 1].Count;
         OnScoreChanged?.Invoke(player, newScore);
         
-        if (newScore >= 2)
+        if (newScore >= GameManager.Instance.GridManager.capturableTilesCount)
         {
             GameManager.Instance.EndGame();
         }
