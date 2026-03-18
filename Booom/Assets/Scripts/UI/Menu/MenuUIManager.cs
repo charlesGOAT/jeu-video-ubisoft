8dfcc85b5fb9efb7391e24867f2f487d202fefa4
camilrondeau <camil.rondeau@gmail.com>
2026-03-18 12:13:09 -0400

    changed menu layout

--- Booom/Assets/Scripts/UI/Menu/MenuUIManager.cs
+++ Booom/Assets/Scripts/UI/Menu/MenuUIManager.cs
@@ -12,6 +12,7 @@ public class MenuUIManager : MonoBehaviour
     
     [SerializeField] private Canvas levelsCanvas;
     [SerializeField] private Image levelPreviewImage;
+    [SerializeField] private TMP_Text levelLabel;
     
     [SerializeField] private Canvas settingsCanvas;
 
