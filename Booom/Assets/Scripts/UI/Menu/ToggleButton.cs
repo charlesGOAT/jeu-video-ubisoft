aa1f2776a66821a6493f0353895c08480090191e
camilrondeau <camil.rondeau@gmail.com>
2026-03-26 14:23:58 -0400

    added missing CVD + kept static values

--- Booom/Assets/Scripts/UI/Menu/ToggleButton.cs
+++ Booom/Assets/Scripts/UI/Menu/ToggleButton.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,11 +13,29 @@ public class ToggleButton : MonoBehaviour
     {
         _toggle = GetComponent<Toggle>();
         _toggle.onValueChanged.AddListener(ValueChanged);
+
+        SetBoolValues();
     }
 
     private void ValueChanged(bool isOn)
     {
         if (_toggle != null)
             trueBackground.SetActive(_toggle.isOn);
+    }
+
+    private void SetBoolValues()
+    {
+        switch (gameObject.name)
+        {
+            case "ToggleItems":
+                _toggle.isOn = LobbyManager.ItemsActivated;
+                break;
+            case "ToggleTuto":
+                _toggle.isOn = LobbyManager.TutorialActivated;
+                break;
+            case "ToggleCVD":
+                _toggle.isOn = LobbyManager.TutorialActivated;
+                break;
+        }
     }
 }
